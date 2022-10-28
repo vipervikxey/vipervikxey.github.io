bc900178dc719dcdc59a96dcb23d4d92359df0c4
viper vikxey <88527795+vipervikxey@users.noreply.github.com>
2022-10-28 16:18:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// Side Navigation Bar
+function openNav() {
+  document.getElementById("Sidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("Sidenav").style.width = "0";
+}
+
+function func() {
+  alert("This site's work is still in progress please visit 'https://www.vipervikxey.blogspot.com' to get codes")
+}
+ 
+ function aboutMe() {
+   alert("soon :)")
+ }
